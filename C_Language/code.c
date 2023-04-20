744e55faca377b1f9273fbb5244747ccd89ce7c8
Mayank <mayanksharmakv@gmail.com>
2023-04-20 22:35:20 +0530

    C_language old code

---
+++ C_Language/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// My fist code in C programming language 1st year
+int main()
+{
+
+    for (int i = 1; i <= 5; i++){
+        printf("hello world\n");
+    }
+
+    return 0;
+}
